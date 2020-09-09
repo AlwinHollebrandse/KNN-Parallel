e4562ac4003bfae0d73f393e4d95a21471f1b7b6
Alwin Hollebrandse <hollebrandsaj@mymail.vcu.edu>
2020-09-09 19:36:14 -0400

    added gather and have the inner loop set up in parallel

--- code/main.cpp
+++ code/main.cpp
@@ -67,10 +67,6 @@ float** getKNNForInstance(int i, int k, float **distancesAndClasses, float **sho
         distancesAndClasses[distancesAndClassesIndex] = row;
     }
 
-    for (int x = 0; x < distancesAndClassesSize; x++) {
-        printf("distancesAndClasses[%d]: distance: %f, class: %f\n", x, distancesAndClasses[x][0], distancesAndClasses[x][1]);
-    }
-
     qsort(distancesAndClasses, distancesAndClassesSize, (2 * sizeof(float)), compare);
 
     for(int j = 0; j < k; j++) {
@@ -107,7 +103,7 @@ int* KNN(ArffData* dataset, int argc, char *argv[]) {
     printf("Hello, world.  I am %d of %d\n", rank, numtasks);
 
     // TODO parallel
-    for(int i = 0; i < 1; i++) {//dataset->num_instances(); i++) { // for each instance in the dataset
+    for(int i = 0; i < dataset->num_instances(); i++) { // for each instance in the dataset
         // printf("threads exist? rank: %d, i: %d", rank, i);
         
         // Scatter the random numbers to all processes
@@ -119,28 +115,26 @@ int* KNN(ArffData* dataset, int argc, char *argv[]) {
         // TODO technocially, this could be outside the outside loop. Both inner and outer would use it.
         int *allThreadInnerIndexesArray = (int*)malloc(sizeof(int) * numtasks * 2);
         if (rank == 0) {
-            printf("instances_per_proc: %d\n", instances_per_proc);
+            // printf("instances_per_proc: %d\n", instances_per_proc);
 
             for (int currentIndex = 0; currentIndex  < 2 * numtasks; currentIndex++) {
                 int threadStartingIndex = 0;
                 if (currentIndex > 0)
                     threadStartingIndex = allThreadInnerIndexesArray[currentIndex - 1];
                 allThreadInnerIndexesArray[currentIndex] = threadStartingIndex;
-                printf("threadStartingIndex: %d\n", threadStartingIndex);
 
                 currentIndex++;
 
                 int threadEndingIndex = allThreadInnerIndexesArray[currentIndex - 1] + instances_per_proc;
-                printf("threadEndingIndex: %d\n", threadEndingIndex);
                 if (threadEndingIndex >= dataset->num_instances())
                     threadEndingIndex = dataset->num_instances(); // set to max possible
                 allThreadInnerIndexesArray[currentIndex] = threadEndingIndex;
             }
 
-            printf("allThreadInnerIndexesArray: ");
-            for (int x = 0; x < numtasks * 2; x++)
-                printf("%d, ", allThreadInnerIndexesArray[x]);
-            printf("\n");
+            // printf("allThreadInnerIndexesArray: ");
+            // for (int x = 0; x < numtasks * 2; x++)
+            //     printf("%d, ", allThreadInnerIndexesArray[x]);
+            // printf("\n");
         }
 
         // printArray (datasetAsMatrix, dataset->num_instances(), dataset->num_attributes(), rank);
@@ -157,71 +151,93 @@ int* KNN(ArffData* dataset, int argc, char *argv[]) {
                     // TODO free subarray
         int startingIndex = threadInnerIndexesArray[0];
         int endingIndex = threadInnerIndexesArray[1];
-        printf("rank: %d, threadInnerIndexesArray: %d, %d\n", rank, startingIndex, endingIndex);
+        // printf("rank: %d, threadInnerIndexesArray: %d, %d\n", rank, startingIndex, endingIndex);
 
         // Compute the kNN of a thread's data
-        int localK = globalK;
-        if (localK > instances_per_proc - 1)
-            localK = instances_per_proc - 1;
-
         int distancesAndClassesSize = endingIndex - startingIndex;
         if (i >= startingIndex && i < endingIndex)
             distancesAndClassesSize--;
         float *distancesAndClasses[distancesAndClassesSize];
-        // float *distancesAndClasses[endingIndex - startingIndex];// - 1]; // NOTE the -1 is there so that the instance in question wont be included // TODO is this a shared or private variable
+
+        // Need to ensure that the 'k' value is never bigger than the amount of data a thread will go through
+        int localK = globalK;
+        if (localK > instances_per_proc)
+            localK = instances_per_proc;
+        if (localK > distancesAndClassesSize)
+            localK = distancesAndClassesSize;
         float *shortestKDistances[localK];
-        // TODO dataset will be changed to non arff type after outer mpi scatter
+
+        // printf("rank: %d, globalK: %d, localK: %d, distancesAndClassesSize: %d\n", rank, globalK, localK, distancesAndClassesSize);
         
-        int number;
-        if (rank == 0) {
-            printf("rank: %d, distancesAndClassesSize: %d\n", rank, distancesAndClassesSize);
-
-            getKNNForInstance(i, localK, distancesAndClasses, shortestKDistances, dataset, startingIndex, endingIndex, distancesAndClassesSize);
-            // printf("rank: %d, instance: ", rank, dataset->get_instance(i)
-            for (int x = 0; x < localK; x++)
-                printf("rank: %d, shortestKDistances: %d, distance: %f, class: %f\n", rank, x, shortestKDistances[x][0], shortestKDistances[x][1]);
-            number = -1;
-
-            MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-            printf("\n\n");
-        }  else if (rank == 1) {
-            MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-            printf("rank: %d, distancesAndClassesSize: %d\n", rank, distancesAndClassesSize);
-
-            getKNNForInstance(i, localK, distancesAndClasses, shortestKDistances, dataset, startingIndex, endingIndex, distancesAndClassesSize);
-            // printf("rank: %d, instance: ", rank, dataset->get_instance(i)
-            for (int x = 0; x < localK; x++)
-                printf("rank: %d, shortestKDistances: %d, distance: %f, class: %f\n", rank, x, shortestKDistances[x][0], shortestKDistances[x][1]);
-        }
-        
-
-
-        
-        // printf("hi2");
-
-        // // Gather all partial averages down to the root process
-        // float *sub_kNNs = NULL;
-
+        getKNNForInstance(i, localK, distancesAndClasses, shortestKDistances, dataset, startingIndex, endingIndex, distancesAndClassesSize);
+        // rdorn@costar.com 
+        // int number;
         // if (rank == 0) {
-        //     sub_kNNs = (float*)malloc(sizeof(float) * floats_per_proc * globalK); // TODO ont be correct size. since its a 2d array that also includes size...
+        //     getKNNForInstance(i, localK, distancesAndClasses, shortestKDistances, dataset, startingIndex, endingIndex, distancesAndClassesSize);
+        //     // printf("rank: %d, instance: ", rank, dataset->get_instance(i)
+        //     for (int x = 0; x < localK; x++)
+        //         printf("rank: %d, shortestKDistances: %d, distance: %f, class: %f\n", rank, x, shortestKDistances[x][0], shortestKDistances[x][1]);
+        //     number = -1;
+
+        //     MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        //     printf("\n\n");
+        // }  else if (rank == 1) {
+        //     MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        //     printf("rank: %d, distancesAndClassesSize: %d\n", rank, distancesAndClassesSize);
+
+        //     getKNNForInstance(i, localK, distancesAndClasses, shortestKDistances, dataset, startingIndex, endingIndex, distancesAndClassesSize);
+        //     // printf("rank: %d, instance: ", rank, dataset->get_instance(i)
+        //     for (int x = 0; x < localK; x++)
+        //         printf("rank: %d, shortestKDistances: %d, distance: %f, class: %f\n", rank, x, shortestKDistances[x][0], shortestKDistances[x][1]);
         // }
-        // MPI_Gather(&sub_kNNs, 1, MPI_FLOAT, sub_kNNs, 1, MPI_FLOAT, 0,
-        //         MPI_COMM_WORLD);
 
-        // // Compute the total average of all numbers.
-        // if (world_rank == 0) {
-        //     qsort(distancesAndClasses, dataset->num_instances() - 1, (2 * sizeof(float)), compare);
-        //     predictions[i] = kVoting(globalK, shortestKDistances);// TODO not using shortestDistances, but the combined of them
-        // }
-        // MPI_Finalize();
+        float *localShortestKDistancesAs1dArray = (float*)malloc(sizeof(float) * localK * 2); // TODO wrong result if rank 0 localK is the smaller one...
 
+        for (int x = 0; x < localK; x++) 
+            for (int y = 0; y < 2; y++)
+                *(localShortestKDistancesAs1dArray + x * 2 + y) = shortestKDistances[x][y];
 
-        // predictions[i] = kVoting(globalK, shortestKDistances);
+        // Gather all partial averages down to the root process
+        float *globalShortestKDistancesAs1dArray = NULL;
+        if (rank == 0)
+            globalShortestKDistancesAs1dArray = (float*)malloc(sizeof(float) * localK * 2 * numtasks); // TODO sometimes, one localK might be 1 smaller than the others... so this could be 1 too big
 
-        // TODO move free
+        MPI_Gather(localShortestKDistancesAs1dArray, localK * 2, MPI_FLOAT, globalShortestKDistancesAs1dArray, localK * 2, MPI_FLOAT, source, MPI_COMM_WORLD);
+
+        // Find the smallest 'k' values of the thread's results
+        if (rank == 0) {
+            // printf("\nlocalShortestKDistancesAs1dArray: ");
+            // for (int x = 0; x < localK * 2; x++) { // TODO wrong result if rank 0 localK is the smaller one...
+            //     printf("%f, ", localShortestKDistancesAs1dArray[x]);
+            // }
+            // printf("\nglobalShortestKDistancesAs1dArray: ");
+            // for (int x = 0; x < localK * 2 * numtasks; x++) { // TODO wrong result if rank 0 localK is the smaller one...
+            //     printf("%f, ", globalShortestKDistancesAs1dArray[x]);
+            // }
+
+            // printf("\nallThreadShortestKDistances: \n");
+            float *allThreadShortestKDistances[localK * numtasks];
+            for (int x = 0; x < localK * numtasks; x++) { // TODO wrong result if rank 0 localK is the smaller one...
+                float *row = (float *)malloc(2 * sizeof(float));
+                for (int y = 0; y < 2; y++)
+                    row[y] = *(globalShortestKDistancesAs1dArray + x * 2 + y);
+                allThreadShortestKDistances[x] = row;
+
+                // printf("%f, %f\n", allThreadShortestKDistances[x][0], allThreadShortestKDistances[x][1]);
+            }
+
+            qsort(allThreadShortestKDistances, localK * numtasks, (2 * sizeof(float)), compare);
+            float *finalShortestKDistances[globalK];
+            for(int j = 0; j < globalK; j++)
+                finalShortestKDistances[j] = allThreadShortestKDistances[j];
+            predictions[i] = kVoting(globalK, finalShortestKDistances);
+            printf("i: %d, class: %d\n", i, predictions[i]);
+        }
+
+        // TODO move free, and have all needed frees
         // for (int j = 0; j < dataset->num_instances() - 1; j++) {
-        //     free(distancesAndClasses[j]);
+        //     free(allThreadShortestKDistances[j]);
         // }
     }
     
